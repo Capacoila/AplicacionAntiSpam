16edbe241dfa1468df4a30121ec1e0cdc363e027
JuanHuamanSanchez <108704131+JuanHuamanSanchez@users.noreply.github.com>
2023-04-24 14:49:48 -0500

    Update and rename Req08.java to numeroverificado.java

--- app/src/main/java/pe/aplicacion/antispam/numeroverificado.java
+++ app/src/main/java/pe/aplicacion/antispam/numeroverificado.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Req08 extends AppCompatActivity {
+public class numeroverificado extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_req08);
+        setContentView(R.layout.activity_numeroverificado);
     }
 }
