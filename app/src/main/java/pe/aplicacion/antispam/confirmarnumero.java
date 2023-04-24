6c0d661e45dd911484a17753fa73fd817f68b090
JuanHuamanSanchez <108704131+JuanHuamanSanchez@users.noreply.github.com>
2023-04-24 14:50:27 -0500

    Update confirmarnumero.java

--- app/src/main/java/pe/aplicacion/antispam/confirmarnumero.java
+++ app/src/main/java/pe/aplicacion/antispam/confirmarnumero.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Req07 extends AppCompatActivity {
+public class confirmarnumero extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
