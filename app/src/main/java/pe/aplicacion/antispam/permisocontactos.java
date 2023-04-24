671797b8d319c03ba46215a7833dda6f45e0e619
Capacoila <122292766+Capacoila@users.noreply.github.com>
2023-04-24 09:38:23 -0500

    Initial commit

---
+++ app/src/main/java/pe/aplicacion/antispam/permisocontactos.java
@@ -0,0 +1,14 @@
+package pe.aplicacion.antispam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class permisocontactos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_permisocontactos);
+    }
+}
---
+++ app/src/main/java/pe/aplicacion/antispam/permisollamadas.java
@@ -0,0 +1,21 @@
+package pe.aplicacion.antispam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class permisollamadas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_permisollamadas);
+    }
+
+    public void onPermitirClick(View view) {
+    }
+
+    public void onNoPermitirClick(View view) {
+    }
+}
