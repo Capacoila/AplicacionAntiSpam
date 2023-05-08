7f298ef5dce719fcda4b2083582b9063eddf04af
Juan Huaman Sanchez <70105432@continental.edu.pe>
2023-05-08 12:04:31 -0500

    Agregado Requerimiento Lista

--- app/src/main/java/pe/aplicacion/antispam/ListaPermisos.java
+++ app/src/main/java/pe/aplicacion/antispam/ListaPermisos.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class BlockPredeterminada extends AppCompatActivity {
+public class ListaPermisos extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_blockpredeterminada);
+        setContentView(R.layout.activity_lista_permisos);
     }
 }
--- app/src/main/java/pe/aplicacion/antispam/SplashBienvenida.java
+++ app/src/main/java/pe/aplicacion/antispam/SplashBienvenida.java
@@ -2,13 +2,38 @@ package pe.aplicacion.antispam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SplashBienvenida extends AppCompatActivity {
+
+    TextView txtLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_bienvenida);
+        txtLogo =findViewById(R.id.txtLogoSplash);
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                llamar_Main(txtLogo);
+            }
+
+            private void llamar_Main(View view) {
+                Intent i = new Intent(SplashBienvenida.this, Comenzar.class);
+                startActivity(i);
+            }
+        },3000);
+
+
+
+
     }
 }
